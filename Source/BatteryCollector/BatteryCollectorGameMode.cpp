67d8783ba17c3fd9a7b64f2cbc664e56c5c49318
Alex Lementuev <a.lementuev@gmail.com>
2016-07-01 21:55:28 -0700

    Added empty 3rd person c++ template project

---
+++ Source/BatteryCollector/BatteryCollectorGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "BatteryCollector.h"
+#include "BatteryCollectorGameMode.h"
+#include "BatteryCollectorCharacter.h"
+
+ABatteryCollectorGameMode::ABatteryCollectorGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
