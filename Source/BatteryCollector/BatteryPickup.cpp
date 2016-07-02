e8d7b346218ddb230fc7380ca8941cb2ad8cd7bc
Alex Lementuev <a.lementuev@gmail.com>
2016-07-01 23:31:38 -0700

    Added 'BatteryPickup' C++ and blueprint classes

---
+++ Source/BatteryCollector/BatteryPickup.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BatteryCollector.h"
+#include "BatteryPickup.h"
+
+//Set default values
+ABatteryPickup::ABatteryPickup()
+{
+	GetMesh()->SetSimulatePhysics(true);
+}
+
+
---
+++ Source/BatteryCollector/BatteryPickup.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Pickup.h"
+#include "BatteryPickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTERYCOLLECTOR_API ABatteryPickup : public APickup
+{
+	GENERATED_BODY()
+	
+public:
+	// Sets default values for this actor's properties
+	ABatteryPickup();
+	
+};
